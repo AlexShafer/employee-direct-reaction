fbfd5f6c33f7c59857bf17b11d3f55f3fb458278
ADS <alexander.douglas.shafer@gmail.com>
2020-10-24 15:23:59 -0700

    Updates

---
+++ precache-manifest.490bf0039d1d4c80a59585d74025b7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e7a7dc9151d128aad3c5123dee25ea",
+    "url": "/employee-direct-reaction/index.html"
+  },
+  {
+    "revision": "9bfc113af387d571affd",
+    "url": "/employee-direct-reaction/static/css/main.4020688a.chunk.css"
+  },
+  {
+    "revision": "f6b6825460b5217278de",
+    "url": "/employee-direct-reaction/static/js/2.298b42e3.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/employee-direct-reaction/static/js/2.298b42e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfc113af387d571affd",
+    "url": "/employee-direct-reaction/static/js/main.413667e8.chunk.js"
+  },
+  {
+    "revision": "e29c36672ba1160b1cf9",
+    "url": "/employee-direct-reaction/static/js/runtime-main.9783ef32.js"
+  }
+]);
